4645bae99baaf063ddca2fe7a47e1320481ec009
karieslexia <E_A_Harrell@hotmail.com>
2020-08-29 22:29:44 -0400

    updating Markdown file, HTML and code with all plots

--- code.R
+++ code.R
@@ -22,5 +22,35 @@ png(filename = "./figure/plot1.png")
 plot(as.Date(Total$date),Total$Total,lwd=5, type="h",xlab = "Date", ylab = "Sum of steps taken")
 dev.off()
 
-?plot
-plot(mydata$interval,mean(mydata$steps,na.rm = TRUE),type = "l")
+
+plot2<-summarise(group_by(mydata,interval), 
+                 Avgsteps=mean(steps, na.rm=TRUE)
+                 )
+plot(plot2$interval,plot2$Avgsteps,type = "l",xlab = "5 minute interval", ylab = "Mean of steps taken")
+
+plot2$interval[plot2$Avgsteps==max(plot2$Avgsteps)]
+
+sum(is.na(mydata))
+
+mydata2<-mydata
+mymean<-mean(mydata2$steps,na.rm = TRUE)
+mydata2$steps[is.na(mydata2$steps)]<- mymean
+
+Total2<-summarise(group_by(mydata2,date), Total=sum(steps))
+png(filename = "./figure/plot3.png")
+plot(as.Date(Total2$date),Total2$Total,lwd=5, type="h",xlab = "Date", ylab = "Sum of steps taken")
+dev.off()
+
+mydata$weekday[weekdays(strptime(mydata$date,"%F"))=="Saturday"]<-"weekend"
+mydata$weekday[weekdays(strptime(mydata$date,"%F"))=="Sunday"]<-"weekend"
+mydata$weekday[weekdays(strptime(mydata$date,"%F"))!="Saturday" & weekdays(strptime(mydata$date,"%F"))!="Sunday" ]<-"weekday"
+mydata$weekday<-factor(c("weekday","weekend"))
+
+mydata3<-mydata %>% 
+  group_by(interval,weekday) %>% 
+  summarise(average = mean(steps, na.rm = TRUE))
+
+library(lattice)
+png(filename = "./figure/plot4.png")
+xyplot(average~interval|weekday,mydata3,layout=c(1,2), ylab = "Number of steps", xlab = "Interval",type="l")
+dev.off()
